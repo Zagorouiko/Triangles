0a1b42259ae701a1a1920d6c783c50c6a9b49625
Ben Cornelis & Leon Zagorouiko <student@epicodus.com>
2015-04-27 10:44:45 -0700

    Make type method work for scalene triangles and non-triangles.

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -9,13 +9,22 @@ describe(Triangle) do
     end
 
     it('Returns isosceles if exactly 2 sides are equal') do
-      test_triangle = Triangle.new(2,2,4)
+      test_triangle = Triangle.new(3,3,4)
       expect(test_triangle.type()).to(eq('isosceles'))
     end
 
     it('Returns isosceles if exactly 2 sides are equal') do
       test_triangle = Triangle.new(3,2,2)
       expect(test_triangle.type()).to(eq('isosceles'))
+    end
+    it('Returns scalene if no sides are equal') do
+      test_triangle = Triangle.new(4,2,3)
+      expect(test_triangle.type()).to(eq('scalene'))
+    end
+
+    it('Returns NOT a triangle if one side is at least as long as the other two combined') do
+      test_triangle = Triangle.new(2,2,8)
+      expect(test_triangle.type()).to(eq('NOT a triangle'))
     end
   end
 
